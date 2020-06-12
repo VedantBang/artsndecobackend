b459a04c5de79d9d726b1404ed1be68b40f4fda7
VedantBang <vedantbang5@gmail.com>
2020-06-12 10:19:31 +0530

    npm commands and mongod configuration file

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ app.use(morgan('dev'));
 app.use('/images', express.static('images'));
 
 app.use((req, res, next) => {
-  	res.set('Access-Control-Allow-Origin', '*');
-  	res.set('Access-Control-Allow-Headers', '*');
-  	res.set('Access-Control-Allow-Methods', '*');
-  	if(req.method === 'OPTIONS'){
-  		res.status(200).end();
-  	} else{
-  		res.set('Content-Type', 'application/json');
-  		next();
-  	}
-  	
+	res.set('Access-Control-Allow-Origin', '*');
+	res.set('Access-Control-Allow-Headers', '*');
+	res.set('Access-Control-Allow-Methods', '*');
+	if(req.method === 'OPTIONS'){
+		res.status(200).end();
+	} else{
+		res.set('Content-Type', 'application/json');
+		next();
+	}
+	
 });
 
 app.use('/display', displayRouter);
@@ -29,16 +29,16 @@ app.use('/user', userRouter);
 app.use('/change', changeRouter);
 
 app.use((req, res, next) => {
-  	let err = new Error("Undefined route");
-  	err.status = 400;
-  	next(err);
+	let err = new Error("Undefined route");
+	err.status = 400;
+	next(err);
 });
 
 app.use((error, req, res, next) => {
-  	res.status(error.status || 500).json({
-    	error: error.message,
-  	});
-  	console.log(error);
+	res.status(error.status || 500).json({
+		error: error.message,
+	});
+	console.log(error);
 });
 
 module.exports = app;
